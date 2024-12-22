cd72fd248945e207c41540e9f9500752f89bdef1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-12-23 04:33:03 +0530

    basic structure

--- src/main/java/org/utils/listeners/MethodInterceptors.java
+++ src/main/java/org/utils/listeners/MethodInterceptors.java
@@ -4,12 +4,17 @@ import org.testng.IMethodInstance;
 import org.testng.IMethodInterceptor;
 import org.testng.ITestContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Mohammad Shahnawaz
+ */
 public class MethodInterceptors implements IMethodInterceptor {
     @Override
     public List<IMethodInstance> intercept(List<IMethodInstance> list, ITestContext iTestContext) {
+        List<IMethodInstance> results=new ArrayList<>();
 
-        return null;
+        return results;
     }
 }
