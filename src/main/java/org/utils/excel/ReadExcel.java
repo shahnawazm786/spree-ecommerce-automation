7fc5fa62a68608f19de3c5b52efa112f14ae2b01
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-12-23 04:33:10 +0530

    basic structure

--- src/main/java/org/utils/excel/ReadExcel.java
+++ src/main/java/org/utils/excel/ReadExcel.java
@@ -3,7 +3,11 @@ package org.utils.excel;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * @author Mohammad Shahnawaz
+ */
 public class ReadExcel {
+
     public List<HashMap<String,String>> getRowsData(String workbookName, String sheetName){
         return null;
     }
