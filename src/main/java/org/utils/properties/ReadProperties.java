d2e0f18d8f20d3cc063152aaf630ac1fa9326cfc
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-12-23 06:01:27 +0530

    basic structure

--- src/main/java/org/utils/properties/ReadProperties.java
+++ src/main/java/org/utils/properties/ReadProperties.java
@@ -4,4 +4,8 @@ package org.utils.properties;
  * @author Mohammad Shahnawaz
  */
 public class ReadProperties {
+    public static final String getData(String key){
+
+        return null;
+    }
 }
