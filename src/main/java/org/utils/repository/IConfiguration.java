fbd2a68c8a26ffd2a4645672df7a1ee6186c9f98
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-12-23 06:01:19 +0530

    basic structure

--- src/main/java/org/utils/repository/IConfiguration.java
+++ src/main/java/org/utils/repository/IConfiguration.java
@@ -7,5 +7,8 @@ public interface IConfiguration {
 //Base file
      String MAIN_BASE_FOLDER="src/java/resources";
      String TEST_BASE_FOLDER="src/test/resources";
+     String BASE_CONFIGURE="/configure";
+     String BASE_DATA="/data";
+     String DATA_XLSX="/excel";
 
 }
