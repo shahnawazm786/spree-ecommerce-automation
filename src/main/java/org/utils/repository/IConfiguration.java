0fd484f9268c6a85905b83014f9dccea6ee397f2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2025-01-23 23:24:27 +0530

    structure development

--- src/main/java/org/utils/repository/IConfiguration.java
+++ src/main/java/org/utils/repository/IConfiguration.java
@@ -4,11 +4,5 @@ package org.utils.repository;
  * @author Mohammad Shahnawaz
  */
 public interface IConfiguration {
-//Base file
-     String MAIN_BASE_FOLDER="src/java/resources";
-     String TEST_BASE_FOLDER="src/test/resources";
-     String BASE_CONFIGURE="/configure";
-     String BASE_DATA="/data";
-     String DATA_XLSX="/excel";
-     String DATA_PROPERTIES="/properties";
+
 }
